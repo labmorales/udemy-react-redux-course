6fca3629843e6456ed069a79fd6195172be91f97
Andre Morales <labmorales@gmail.com>
2020-06-09 23:59:27 -0300

    Stream App Finishedgit status

--- streams/client/src/components/Streams/StreamShow.js
+++ streams/client/src/components/Streams/StreamShow.js
@@ -1,10 +1,45 @@
 import React, { Component } from "react";
 import { fetchStream } from "../../actions";
 import { connect } from "react-redux";
+import flvjs from "flv.js";
 
 export class StreamShow extends Component {
+  constructor(props) {
+    super(props);
+
+    this.videoRef = React.createRef();
+  }
+
   componentDidMount = () => {
-    this.props.fetchStream(this.props.match.params.id);
+    const { id } = this.props.match.params;
+    this.props.fetchStream(id);
+
+    this.buildPlayer();
+  };
+
+  componentWillUnmount() {
+    if (this.player) {
+      this.player.destroy();
+    }
+  }
+
+  componentDidUpdate() {
+    this.buildPlayer();
+  }
+
+  buildPlayer = () => {
+    if (this.player || !this.props.stream) {
+      return;
+    }
+
+    const { id } = this.props.match.params;
+
+    this.flvPlayer = flvjs.createPlayer({
+      type: "flv",
+      url: `http://localhost:8000/live/${id}.flv`,
+    });
+    this.flvPlayer.attachMediaElement(this.videoRef.current);
+    this.flvPlayer.load();
   };
 
   render() {
@@ -16,6 +51,7 @@ export class StreamShow extends Component {
 
     return (
       <div>
+        <video ref={this.videoRef} style={{ width: "100%" }} controls></video>
         <h1>{title}</h1>
         <h3>{description}</h3>
       </div>
